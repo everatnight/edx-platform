6c0d29b366fc359a7692160fdc2d044897113ecf
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-07-02 14:19:44 -0400

    updated files to match post-merge status **to squash**

--- lms/urls.py
+++ lms/urls.py
@@ -112,14 +112,15 @@ urlpatterns = ('',  # nopep8
     # Favicon
     (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
 
-    url(r'^submit_feedback$', 'util.views.submit_feedback_via_zendesk'),
-
     # TODO: These urls no longer work. They need to be updated before they are re-enabled
     # url(r'^reactivate/(?P<key>[^/]*)$', 'student.views.reactivation_email'),
 
     # Search
     url(r'^search$', 'search.views.search')
+<<<<<<< HEAD
 
+=======
+>>>>>>> 4a96caa... search result vertical linking in place, mongo tree traversal also working
 )
 
 if settings.PERFSTATS:
